42fcea168e72cc37b511562fdd7bf3312f1c949f
Alex3737 <alexbowman66@gmail.com>
2019-01-04 20:44:33 -0500

    Adding the Project

---
+++ app/src/main/java/hyland2019/blackmirror/MainActivity.java
@@ -0,0 +1,13 @@
+package hyland2019.blackmirror;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
